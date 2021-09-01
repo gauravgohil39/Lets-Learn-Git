eaf78f8ccd18e2597b4bde21996f1530c3dec40a
Gaurav Gohil <gaurav.gohil39@gmail.com>
2021-09-01 12:06:32 +0530

    adding Add.java, Diff.java, Multiply.java

--- Add.java
+++ Add.java
@@ -1,8 +1,8 @@
-public class Diff {
+public class Add {
     public static void main(String[] args) {
         int a = 4;
         int b = 2;
 
-        System.out.println("the product is "+(a-b));
+        System.out.println("the product is "+(a+b));
     }
 }
--- Multiply.java
+++ Multiply.java
@@ -1,8 +1,9 @@
-public class Diff {
+public class Multiply {
+
     public static void main(String[] args) {
         int a = 4;
         int b = 2;
 
-        System.out.println("the product is "+(a-b));
+        System.out.println("the product is "+(a*b));
     }
 }
